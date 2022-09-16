f6c589163d88a89d9f5f733b2ed8637bc406e33a
Fiamma Muscari <fiammamuscari@gmail.com>
2022-09-16 19:24:38 -0300

    [Update]:fixing deploy

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,8 @@
 // Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore/lite";
-import {getStorage} from 'firebase/storage'
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getStorage } from 'firebase/storage';
 import { getEnvironment } from "../notes/helpers/getEnvironment";
 // import { getAnalytics } from "firebase/analytics";
 
@@ -15,15 +14,14 @@ import { getEnvironment } from "../notes/helpers/getEnvironment";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const{
+const {
   VITE_APIKEY,
   VITE_AUTHDOMAIN,
   VITE_PROJECTID,
   VITE_STORAGEBUCKET,
   VITE_MESSAGINGSENDERID,
   VITE_APPID,
-} = getEnvironment()
-
+} = getEnvironment();
 
 const firebaseConfig = {
   apiKey: VITE_APIKEY,
@@ -32,10 +30,8 @@ const firebaseConfig = {
   storageBucket: VITE_STORAGEBUCKET,
   messagingSenderId: VITE_MESSAGINGSENDERID,
   appId: VITE_APPID,
-
-//   measurementId: "G-NKK52J4LGV"
-
 };
+
 
 
 // Initialize Firebase
@@ -43,6 +39,6 @@ const firebaseConfig = {
 const FirebaseApp = initializeApp(firebaseConfig);
 
 // const analytics = getAnalytics(app);
-export const FirebaseAuth = getAuth(FirebaseApp)
+export const FirebaseAuth = getAuth( FirebaseApp );
 export const FirebaseDB = getFirestore( FirebaseApp );
 export const FirebaseStorage = getStorage( FirebaseApp );
